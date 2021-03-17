94ad1b784ae0de9a48f9187db17b9fea156613db
VivianeSouza923 <viviane.souza05@aluno.ifce.edu.br>
2021-03-17 16:12:13 -0300

    update

---
+++ tempMatchingTracking.py
@@ -0,0 +1,20 @@
+# ! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import os
+import sys
+import time
+import cv2
+import numpy as numpy
+from math import *
+from skimage.feature import local_binary_pattern
+
+
+def normalozeImage(v):
+    v = (v - v.min()) / (v.max() - v.min() + 1)
+    result = (v * 255)astype(np.uint8)
+    return result
+
+
+cap = cv2.VideoCapture('bread.mp4')
